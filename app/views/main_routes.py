ee783e129b59737ba2c428aba21177dda77a036c
a <>
2024-11-14 20:38:40 -0500

    update url

--- app/views/main_routes.py
+++ app/views/main_routes.py
@@ -3,4 +3,4 @@ from app import app
 
 @app.route("/")
 def index():
-    return "This is a basic API server created for my <a href=\"https://github.com/ChristopherLi05/CSC207-Project\">CSC207 Project</a><br>Here is the <a href=\"\">source code</a>"
+    return "This is a basic API server created for my <a href=\"https://github.com/ChristopherLi05/CSC207-Project\">CSC207 Project</a><br>Here is the <a href=\"https://github.com/ChristopherLi05/CSC207-Project-Server\">source code</a>"
